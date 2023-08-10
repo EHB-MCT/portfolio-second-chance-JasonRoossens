d339175bccfd0bbf55f8dec6745c330d7db586ae
JasonRoossens <jason.roossens@student.ehb.be>
2023-08-10 14:55:02 +0200

    Moved database connection to db.js file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 7001;
+const connectDB = require('./config/db'); // Import the database connection function
 
 // Middleware setup
 app.use(express.static('public'));
-app.use(express.json()); // Use built-in JSON parsing middleware
+app.use(express.json());
 app.use(cors());
 
 // Basic route
@@ -16,15 +14,10 @@ app.get('/', (req, res) => {
   res.status(300).redirect('/info.html');
 });
 
-// Connect to MongoDB
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Start the server after the database connection is established
-    app.listen(port, () => {
-      console.log(`API is listening at http://localhost:${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+// Call the database connection function
+connectDB().then(() => {
+  // Start the server after the database connection is established
+  app.listen(port, () => {
+    console.log(`API is listening at http://localhost:${port}`);
   });
+});
