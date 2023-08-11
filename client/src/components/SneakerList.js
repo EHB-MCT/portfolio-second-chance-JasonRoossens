aa28745e986b0640e7974fbe7418faa1b334f9ac
JasonRoossens <jason.roossens@student.ehb.be>
2023-08-11 17:15:42 +0200

    Added create sneaker

--- client/src/components/SneakerList.js
+++ client/src/components/SneakerList.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 function SneakerList() {
   const [sneakers, setSneakers] = useState([]);
+  const [newSneaker, setNewSneaker] = useState({
+    brand: '',
+    model: '',
+    color: '',
+    price: 0,
+  });
 
   useEffect(() => {
-    fetch('http://localhost:7001/api/sneakers') // Update the endpoint
+    fetch('http://localhost:7001/api/sneakers')
       .then(response => response.json())
       .then(data => {
         setSneakers(data);
@@ -13,6 +19,39 @@ function SneakerList() {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setNewSneaker(prevSneaker => ({
+      ...prevSneaker,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // Perform API call to create a new sneaker
+    fetch('http://localhost:7001/api/sneakers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newSneaker),
+    })
+      .then(response => response.json())
+      .then(createdSneaker => {
+        setSneakers(prevSneakers => [...prevSneakers, createdSneaker]);
+        setNewSneaker({
+          brand: '',
+          model: '',
+          color: '',
+          price: 0,
+        });
+      })
+      .catch(error => {
+        console.error('Error creating sneaker:', error);
+      });
+  };
 
   return (
     <div>
@@ -27,6 +66,27 @@ function SneakerList() {
           </li>
         ))}
       </ul>
+      
+      <h2>Create a New Sneaker</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Brand:
+          <input type="text" name="brand" value={newSneaker.brand} onChange={handleInputChange} />
+        </label>
+        <label>
+          Model:
+          <input type="text" name="model" value={newSneaker.model} onChange={handleInputChange} />
+        </label>
+        <label>
+          Color:
+          <input type="text" name="color" value={newSneaker.color} onChange={handleInputChange} />
+        </label>
+        <label>
+          Price:
+          <input type="number" name="price" value={newSneaker.price} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Create Sneaker</button>
+      </form>
     </div>
   );
 }
